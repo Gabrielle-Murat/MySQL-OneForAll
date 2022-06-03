08a2bfc1cbefc0e3e0077a4a2cb0a7c9ab7cf383
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-03 16:14:20 -0300

    Cria e popula a tabela users

--- desafio1.sql
+++ desafio1.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE SpotifyClone;
 
 CREATE TABLE SpotifyClone.Subscriptions(
-  subscriptions_id INT PRIMARY KEY AUTO_INCREMENT,
+  subscription_id INT PRIMARY KEY AUTO_INCREMENT,
   subscription_name VARCHAR(30) NOT NULL,
   subscription_value DECIMAL(3,2) NOT NULL,
 ) ENGINE=InnoDB;
@@ -30,3 +30,26 @@ VALUES
   ('Freedie Shannon'), -- id: 4
   ('Tyler Isle'), -- id: 5
   ('Fog'); -- id: 6
+
+CREATE TABLE SpotifyClone.Users(
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_name VARCHAR(40) NOT NULL,
+  user_age TINYINT NOT NULL,
+  subscription_id INT NOT NULL,
+  subscription_date DATE NOT NULL,
+  FOREIGN KEY (subscription_id) REFERENCES SpotifyClone.Subscriptions(subscription_id)
+) ENGINE=InnoDB;
+
+INSERT INTO
+  SpotifyClone.Users
+VALUES
+  ('Thati', 23, 4, 2019-10-20), -- id: 1
+  ('Cintia', 35, 2, 2017-12-30), -- id: 2
+  ('Bill', 20, 3, 2019-06-05), -- id: 3
+  ('Roger', 45, 1, 2020-05-13), -- id: 4
+  ('Norman', 58, 1, 2017-02-17), -- id: 5
+  ('Patrick', 33, 2, 2017-01-06), -- id: 6
+  ('Vivian', 26, 3, 2018-01-05), -- id: 7
+  ('Carol', 19, 3, 2018-02-14), -- id: 8
+  ('Angelina', 42, 2, 2018-04-29), -- id: 9
+  ('Paul', 46, 2, 2017-01-17); -- id: 10
