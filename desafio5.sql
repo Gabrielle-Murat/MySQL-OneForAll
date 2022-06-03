9aaa8945a93167746fe92c8618883cf5c4df9971
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-03 18:40:07 -0300

    Add desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,19 @@
+SELECT
+	son.song_title AS 'cancao',
+    COUNT(DISTINCT usr.user_id) AS 'reproducoes'
+FROM
+	SpotifyClone.Songs AS son
+INNER JOIN
+	SpotifyClone.Reproduction_Histories AS rep
+ON
+	son.song_id = rep.song_id
+INNER JOIN
+	SpotifyClone.Users AS usr
+ON
+	usr.user_id = rep.user_id
+GROUP BY
+	son.song_title
+ORDER BY
+	reproducoes DESC,
+    cancao ASC
+LIMIT 2;
