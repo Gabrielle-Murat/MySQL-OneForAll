46724fcd809cd8787ed404c02a32929aee866d0b
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-03 16:04:13 -0300

    Cria e popula a tabela artists

--- desafio1.sql
+++ desafio1.sql
@@ -15,3 +15,18 @@ VALUES
   ('familiar', 7.99), -- id: 2
   ('universitário', 5.99), -- id: 3
   ('gratuito', 0.00); -- id: 4
+
+CREATE TABLE SpotifyClone.Artists(
+  artist_id INT PRIMARY KEY AUTO_INCREMENT,
+  artist_name VARCHAR(50) NOT NULL,
+) ENGINE=InnoDB;
+
+INSERT INTO
+  SpotifyClone.Artists
+VALUES
+  ('Walter Phoenix'), -- id: 1
+  ('Peter Strong'), -- id: 2
+  ('Lance Day'), -- id: 3
+  ('Freedie Shannon'), -- id: 4
+  ('Tyler Isle'), -- id: 5
+  ('Fog'); -- id: 6
